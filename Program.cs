1af930859a45ed3fd42b5c05ce160c77a3c9cafd
ZroViiii <128241988+ZroViiii@users.noreply.github.com>
2023-03-19 12:21:00 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,10 +12,12 @@ namespace PingPong3
     {
         static int height = 20;//Chiều cao border
         static int width = 80;// Chiều rộng border
-        static int X=20;//Vị trí border theo cột
-        static int Y=6;//Vị trí border theo dòng
-        static int positionP1 = Y+7;//Vị trí của Player1
-        static int positionP2 = Y+7;//Vị trí của Player2
+        static int x=20;//Vị trí border theo cột
+        static int y=6;//Vị trí border theo dòng
+        static int xP1;//Vị trí của Player1 theo cột
+        static int xP2;//Vị trí của Player1 theo cột
+        static int yP1 = y +7;//Vị trí của Player1 theo dòng
+        static int yP2 = y +7;//Vị trí của Player2 theo dòng
         static int lengthP = 6;
         static ConsoleKey consoleKey;
 
@@ -35,8 +37,8 @@ namespace PingPong3
                     Console.Write(i);
                 dem++;
             }
-            
             Console.CursorVisible= false;
+
             Whole();
             
         }
@@ -57,34 +59,34 @@ namespace PingPong3
             //Dòng trên
             for (int i = 0; i < width - 2; i++) 
             {
-                Console.SetCursorPosition(X+1+i,Y);
+                Console.SetCursorPosition(x+1+i,y);
                 Console.Write("─");
             }
             //Dòng dưới
             for (int i = 0; i < width - 2; i++)
             {
-                Console.SetCursorPosition(X+1+i,Y+height-1);
+                Console.SetCursorPosition(x +1+i,y +height-1);
                 Console.Write("─");
             }
             //Hàng trái
             for (int i = 0; i < height - 2; i++)
             {
-                Console.SetCursorPosition(X,Y+1+i);
+                Console.SetCursorPosition(x,y +1+i);
                 Console.Write("│");
             }
             //Hàng phải
             for (int i = 0; i < height - 2; i++)
             {
-                Console.SetCursorPosition(X+width-1,Y + 1 + i);
+                Console.SetCursorPosition(x +width-1,y + 1 + i);
                 Console.Write("│");
             }
-            Console.SetCursorPosition(X, Y);
+            Console.SetCursorPosition(x, y);
             Console.Write("┌");
-            Console.SetCursorPosition(X+width - 1, Y);
+            Console.SetCursorPosition(x +width - 1, y);
             Console.Write("┐");
-            Console.SetCursorPosition(X,Y+ height - 1);
+            Console.SetCursorPosition(x,y + height - 1);
             Console.Write("└");
-            Console.SetCursorPosition(X+width - 1, Y + height - 1);
+            Console.SetCursorPosition(x +width - 1, y + height - 1);
             Console.Write("┘");
         }
         static void Player1()
@@ -92,7 +94,7 @@ namespace PingPong3
             Console.BackgroundColor = ConsoleColor.Green;
             for (int i = 0; i < lengthP; i++)
             {
-                Console.SetCursorPosition(X+1,positionP1+i);
+                Console.SetCursorPosition(x +1,yP1+i);
                 Console.WriteLine(" ");
             }
             Console.BackgroundColor = ConsoleColor.Black;
@@ -102,14 +104,13 @@ namespace PingPong3
             Console.BackgroundColor = ConsoleColor.Green;
             for (int i = 0; i < lengthP; i++)
             {
-                Console.SetCursorPosition(X+width - 2,positionP2+i);
+                Console.SetCursorPosition(x +width - 2,yP2+i);
                 Console.WriteLine(" ");
             }
             Console.BackgroundColor = ConsoleColor.Black;
         }
         static void Show()
         {
-            
             Border();
             Player1();
             Player2();
@@ -118,40 +119,40 @@ namespace PingPong3
         #region Move
         static void UpP1()
         {
-            positionP1--;
-            if (positionP1 < Y+1) positionP1++;
+            yP1--;
+            if (yP1 < y +1) yP1++;
             Player1();
-            Console.SetCursorPosition(X+1, positionP1 + lengthP);
+            Console.SetCursorPosition(x +1, yP1 + lengthP);
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Write(" ");
         }
         static void DownP1()
         {
-            Console.SetCursorPosition(X+1, positionP1);
+            Console.SetCursorPosition(x +1, yP1);
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Write(" ");
-            positionP1++;
-            if (positionP1 + lengthP > Y + height -1) 
-                positionP1--;
+            yP1++;
+            if (yP1 + lengthP > y + height -1) 
+                yP1--;
             Player1();
         }
         static void UpP2()
         {
-            positionP2--;
-            if (positionP2 < Y+1) positionP2++;
+            yP2--;
+            if (yP2 < y +1) yP2++;
             Player2();
-            Console.SetCursorPosition(X+width - 2,positionP2 + lengthP);
+            Console.SetCursorPosition(x +width - 2,yP2 + lengthP);
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Write(" ");
         }
         static void DownP2()
         {
-            Console.SetCursorPosition(X + width - 2, positionP2);
+            Console.SetCursorPosition(x + width - 2, yP2);
             Console.BackgroundColor = ConsoleColor.Black;
             Console.Write(" ");
-            positionP2++;
-            if (positionP2 + lengthP > Y+ height -1) 
-                positionP2--;
+            yP2++;
+            if (yP2 + lengthP > y + height -1) 
+                yP2--;
             Player2();
         }
         #endregion
